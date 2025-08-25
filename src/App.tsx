9dc58a544e574899c74c34f9c3837103bfb0101f
LeonArial <947113723@qq.com>
2025-08-25 18:14:17 +0800

    v6.2 -npm打包bug修复

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/daily" element={<Converter />} />
+          <Route path="/report" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
